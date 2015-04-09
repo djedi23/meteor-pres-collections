d1f40cd85b765cd39663491bdc192413a813a49e
Moïse Valvassori <moise.valvassori@gmail.com>
2015-04-09 06:24:39 +0200

    formatting

--- server/publish.js
+++ server/publish.js
@@ -1,13 +1,13 @@
 Meteor.publish("presentationsByName", function (presentation) {
-    check(presentation, String);
+  check(presentation, String);
 
-    var presentations = modules.collections.Presentations.find({name:presentation});
-    var prids = presentations.map(function(p) {return p.session;});
-    var sessions = modules.collections.Sessions.find({_id: {$in: prids}});
-    
-//    console.log(prids);
-    return [
-	presentations,
-	sessions
-    ];
+  var presentations = modules.collections.Presentations.find({name:presentation});
+  var prids = presentations.map(function(p) {return p.session;});
+  var sessions = modules.collections.Sessions.find({_id: {$in: prids}});
+  
+  //    console.log(prids);
+  return [
+    sessions,
+    presentations
+  ];
 });
